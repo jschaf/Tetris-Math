d84480775f82da0c8ae7f953736ef15b099ea29a
Joe <joe@joe-vm.(none)>
2010-01-29 17:11:13 -0500

    Added textView.py.

---
+++ textView.py
@@ -0,0 +1,10 @@
+'''A view for the command line.'''
+
+class TextView(object):
+    '''A view primarily to aid in debugging.'''
+
+    def __init__(self, board):
+        pass
+
+    def draw(self):
+        pass
