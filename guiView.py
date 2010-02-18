86154f08acae5e434fecbba73f621f23037e5574
Joe <joe@joe-vm.(none)>
2010-02-17 23:33:54 -0500

    Added font, screen and surface declarations and some missing parameters.

--- guiView.py
+++ guiView.py
@@ -3,17 +3,21 @@
 
 import pygame
 from pygame.locals import *
+import equation
 
 class GuiView(object):
     '''The primary gui view.'''
 
     def __init__(self, board):
         self.font = pygame.font.Font(None, 36)
-        self.equation = None
+        self.equation = equation.Equation(0)
+        
+        self.screen = pygame.display.set_mode((400,450))
+        self.font = pygame.font.Font(None, 36)
 
         # set up the background surface
-        self.surface = pygame.Surface(screen.get_size())
-        self.surface = background.convert()
+        self.surface = pygame.Surface(self.screen.get_size())
+        self.surface = self.surface.convert()
         self.surface.fill((250, 250, 250))
 
 
@@ -24,13 +28,13 @@ class GuiView(object):
     def draw(self):
 
         # (text, smoothed(1=true), text RGB color)
-        text = font.render(self.equation.render(), 1, (31,73,125))
+        text = self.font.render(self.equation.render(), 1, (31,73,125))
 
         # clear the board and copy the rendered message onto it
         # (RGB color)
-        board.fill ((250, 250, 250))
-        board.blit(text, (130, 200)) #(x and y location)
+        self.surface.fill ((250, 250, 250))
+        self.surface.blit(text, (130, 200)) #(x and y location)
 
-        self.surface.blit (board, (0, 0))
+        self.surface.blit (self.surface, (0, 0))
         pygame.display.flip()
 
