64b7d917984228bd030c6129b0d540f273b75872
Joe <joe@joe-vm.(none)>
2010-02-17 23:34:44 -0500

    Replaced None with an Equation.

--- textView.py
+++ textView.py
@@ -1,14 +1,16 @@
 '''A view for the command line.'''
 
+import equation
+
 class TextView(object):
     '''A view primarily to aid in debugging.'''
 
     def __init__(self, board):
-        self.equation = None
+        self.equation = equation.Equation(0)
 
 
     def draw(self):
-        print(equation.render())
+        print(self.equation.render())
 
     def update_equation(self, equation):
         self.equation = equation
