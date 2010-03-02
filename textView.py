dddd924eef4febfc6b6a4c0149988e9896c5267c
Joe <joe@joe-vm.(none)>
2010-03-01 23:39:08 -0500

    Added update_eqn.

--- textView.py
+++ textView.py
@@ -6,12 +6,15 @@ class TextView(object):
     '''A view primarily to aid in debugging.'''
 
     def __init__(self, board):
-        self.equation = equation.Equation(0)
+        self.equation = None
+        self.changed_eqn = False
 
-
+    def update_eqn(self, new_eqn):
+        self.equation = new_eqn
+        self.changed_eqn = True
+        
     def draw(self):
-        print(self.equation.render())
-
-    def update_equation(self, equation):
-        self.equation = equation
+        if self.changed_eqn:
+            print(self.equation.render())
+            self.changed_eqn = False
 
