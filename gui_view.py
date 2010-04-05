512d722c6de5b57cc2e995b421bf98578cfc7491
Joe <joe@joe-vm.(none)>
2010-04-04 23:37:08 -0400

    Made individual draw methods for different modes.

--- gui_view.py
+++ gui_view.py
@@ -33,39 +33,13 @@ class GuiView(object):
     # draws the problem in the window and also puts it on the command line
     def draw(self, mode):
         if mode == "welcome":
-            welcome_msg = self.font.render("Welcome to Tetris Math", 1, (31,73,125), (250,250,250))
-            instruction = self.font.render("Press a number from the options below:", 1, (31,73,125), (250,250,250))
-            choice1 = self.font.render("1. Single Player", 1, (31,73,125), (250,250,250))
-            choice2 = self.font.render("2. Multi Player", 1, (31,73,125), (250,250,250))
-            choice3 = self.font.render("3. Practice Quiz", 1, (31,73,125), (250,250,250))
-            self.surface.fill ((250, 250, 250))
-            self.surface.blit(welcome_msg, (130, 200))
-            self.surface.blit(instruction, (130, 240))
-            self.surface.blit(choice1, (130, 270))
-            self.surface.blit(choice2, (130, 300))
-            self.surface.blit(choice3, (130, 330))
+            self._draw_welcome()
+
         elif mode == "running":        
-            if self.changed_eqn:
-                if self.board.current_input:
-                    guess_num = int_from_digits(self.board.current_input)
-                    eqn_render = self.equation.render(guess=guess_num)
-                    print eqn_render
-                else:
-                    eqn_render = self.equation.render()
-                
-                # (text, smoothed(1=true), text RGB color)    
-                text = self.font.render(eqn_render, 1, (31,73,125), (250,250,250))
-                
-                # clear the board and copy the rendered message onto it
-                self.changed_eqn = False
-                
-                self.surface.fill ((250, 250, 250))
-                self.surface.blit(text, (130, 200)) #(x and y location)
-                
-                text_rect = pygame.Rect(120, 190, 130, 40)
-                border = pygame.draw.rect(self.surface, Color('red'), text_rect, 1)
+            self._draw_running()
+
         elif mode == "summary":
-            summary_string = "Summary:\nYou answered {0} of {1} problems correctly"
+            summary_string = "You answered {0} of {1} problems correctly"
             text = self.font.render(summary_string.format(self.board.correct_tally, 
                                                           self.board.problem_count),
                                      1, (31,73,125), (250,250,250))
@@ -75,7 +49,40 @@ class GuiView(object):
         self.screen.blit (self.surface, (0, 0))        
         pygame.display.flip()
     
-    
+    def _draw_welcome(self):
+        welcome_msg = self.font.render("Welcome to Tetris Math", 1, (31,73,125), (250,250,250))
+        instruction = self.font.render("Press a number from the options below:", 1, (31,73,125), (250,250,250))
+        choice1 = self.font.render("1. Single Player", 1, (31,73,125), (250,250,250))
+        choice2 = self.font.render("2. Multi Player", 1, (31,73,125), (250,250,250))
+        choice3 = self.font.render("3. Practice Quiz", 1, (31,73,125), (250,250,250))
+        self.surface.fill ((250, 250, 250))
+        self.surface.blit(welcome_msg, (130, 200))
+        self.surface.blit(instruction, (130, 240))
+        self.surface.blit(choice1, (130, 270))
+        self.surface.blit(choice2, (130, 300))
+        self.surface.blit(choice3, (130, 330))
+
+    def _draw_running(self):
+        # if self.changed_eqn:
+        if self.board.current_input:
+            guess_num = int_from_digits(self.board.current_input)
+            eqn_render = self.equation.render(guess=guess_num)
+            print(eqn_render)
+        else:
+            eqn_render = self.equation.render()
+
+        # (text, smoothed(1=true), text RGB color)    
+        text = self.font.render(eqn_render, 1, (31,73,125), (250,250,250))
+
+        # clear the board and copy the rendered message onto it
+        self.changed_eqn = False
+
+        self.surface.fill ((250, 250, 250))
+        self.surface.blit(text, (30, self.board.current_eqn_position))
+
+        text_rect = pygame.Rect(120, 190, 130, 40)
+        border = pygame.draw.rect(self.surface, Color('red'), text_rect, 1)
+
     def clear_screen(self):
         self.surface.fill((250,250,250))
         self.screen.blit(self.surface, (0, 0))
