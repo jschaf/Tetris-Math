c0b7cd03f3dd3f6368c5fa6d440f8ee3b288770a
Joe <joe@joe-vm.(none)>
2010-04-04 23:38:51 -0400

    Added a clock with a timer.

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def main():
     pygame.init()
     pygame.display.set_caption('Tetris Math')
 
+    clock = pygame.time.Clock()
+    MAX_FPS = 40
     controller = GameController()
-
+    
     controller.running = True
 
-    while controller.running:        
+    while controller.running:
         controller.update()
         controller.draw()
-            
+        clock.tick(MAX_FPS)
+
 if __name__ == '__main__':
     main()
